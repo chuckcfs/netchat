ffe1531157c64fd1d7525371aabaca380ea3ef60
Carlos Cessa <carlos@pixative.com>
2015-02-02 14:22:35 -0600

    Chat search
    - Modified chat search functionality to avoid obtaining the own user
    name when searching for chats

--- app/js/chat/ChatList.js
+++ app/js/chat/ChatList.js
@@ -58,7 +58,10 @@ define( function ( require ) {
         $scope.search   = function () {
             User.query({
                 filters : {
-                    name    : $scope.keyword
+                    name    : $scope.keyword,
+                    _id     : {
+                        '$ne'   : $scope.user_id
+                    }
                 },
                 limit   : 20,
                 select  : 'name'
